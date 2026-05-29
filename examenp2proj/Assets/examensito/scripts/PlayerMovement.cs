ff5eeafdc1b831dca835fc8a23b53cfe5fb8c631
Francesca Vera <sally.acorn16@hotmail.com>
2026-05-28 20:33:08 -0500

    el jugador ya tiene cámra
    no puede ser que me demorara tanto en la cámara, gg profe,

--- examenp2proj/Assets/examensito/scripts/PlayerMovement.cs
+++ examenp2proj/Assets/examensito/scripts/PlayerMovement.cs
@@ -19,17 +19,32 @@ public class PlayerMovement : MonoBehaviour
 
     void Update()
     {
-        
+        // inputs
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
 
-        // guardam direccion
-        movement = new Vector3(horizontal, 0f, vertical);
+        // movimiento relativo a la camara
+        movement =
+            transform.forward * vertical +
+            transform.right * horizontal;
+
+        // evitar velocidad diagonal absurda
+        movement.Normalize();
+
+
+        // hago que el player rote igual que la camara
+        // porque el modelo se quedaba tieso mientras la camara giraba
+        // y no tenia sentido
+        transform.rotation = Quaternion.Euler(
+            0f,
+            Camera.main.transform.eulerAngles.y,
+            0f
+        );
     }
 
     void FixedUpdate()
     {
-        // mueve al jugador
+        // mover jugador usando fisicas
         rb.MovePosition(
             rb.position +
             movement * moveSpeed * Time.fixedDeltaTime
